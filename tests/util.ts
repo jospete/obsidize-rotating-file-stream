cb61b45112ee273ae96f7f13782c69261800500b
Josh Noel <obsidize@gmail.com>
2021-03-14 16:34:53 -0500

    fix util exports

--- tests/util.ts
+++ tests/util.ts
@@ -1,12 +1,12 @@
-const dumpHex = (bytes: number[]): string => {
+export const dumpHex = (bytes: number[]): string => {
 	return bytes.map(b => b.toString(16).toUpperCase().padStart(2, '0')).join(' ');
 };
 
-const dumpHexBuffer = (buffer: ArrayBuffer): string => {
+export const dumpHexBuffer = (buffer: ArrayBuffer): string => {
 	return dumpHex(Array.from(new Uint8Array(buffer)));
 };
 
-const generateRandomBuffer = (byteLength: number): ArrayBuffer => {
+export const generateRandomBuffer = (byteLength: number): ArrayBuffer => {
 
 	const bytes = [];
 
@@ -17,22 +17,21 @@ const generateRandomBuffer = (byteLength: number): ArrayBuffer => {
 	return Uint8Array.from(bytes).buffer;
 };
 
-
-const bufferFromText = async (text: string): Promise<ArrayBuffer> => {
+export const bufferFromText = async (text: string): Promise<ArrayBuffer> => {
 	return new TextEncoder().encode(text).buffer;
 };
 
-const bufferFromBlob = async (blob: Blob): Promise<ArrayBuffer> => {
+export const bufferFromBlob = async (blob: Blob): Promise<ArrayBuffer> => {
 	return blob.arrayBuffer();
 };
 
-const bufferFrom = async (value: string | Blob | ArrayBuffer): Promise<ArrayBuffer> => {
+export const bufferFrom = async (value: string | Blob | ArrayBuffer): Promise<ArrayBuffer> => {
 	if (typeof value === 'string') return bufferFromText(value as string);
 	if (value instanceof Blob) return bufferFromBlob(value as Blob);
 	return value;
 };
 
-const getBytes = (buffer: ArrayBuffer): number[] => {
+export const getBytes = (buffer: ArrayBuffer): number[] => {
 	return buffer
 		&& buffer instanceof ArrayBuffer
 		&& buffer.byteLength > 0
@@ -40,7 +39,7 @@ const getBytes = (buffer: ArrayBuffer): number[] => {
 		: [];
 };
 
-const bufferConcat = (a: ArrayBuffer, b: ArrayBuffer): ArrayBuffer => {
+export const bufferConcat = (a: ArrayBuffer, b: ArrayBuffer): ArrayBuffer => {
 	const bytes = getBytes(a).concat(getBytes(b)).filter(v => typeof v === 'number');
 	return Uint8Array.from(bytes).buffer;
 };
